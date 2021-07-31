597f9fd183efb5ae90a5e4e08a650f90b64ceaff
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-07-31 15:30:44 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,20 @@
 const _pi = 3.141;
 const _2b2tseed = -4172144997902289642;
+
+function customTag(tagName,fn){
+  document.createElement(tagName);
+  //find all the tags occurrences (instances) in the document
+  var tagInstances = document.getElementsByTagName(tagName);
+        //for each occurrence run the associated function
+        for ( var i = 0; i < tagInstances.length; i++) {
+            fn(tagInstances[i]);
+        }
+}
+ 
+function lol(element){
+        //code for rendering the element goes here
+// W.I.P tag
+        }
+}   
+ 
+customTag("lol",lol);
