e53f9f3db97059b8507a6f971fc632865ff84c5b
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-08-10 18:00:44 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,9 @@
+// Just some random variables.
 const _pi = 3.141;
 const _2b2tseed = -4172144997902289642;
 
-function customTag(tagName,fn){
-  document.createElement(tagName);
-  //find all the tags occurrences (instances) in the document
-  var tagInstances = document.getElementsByTagName(tagName);
-        //for each occurrence run the associated function
-        for ( var i = 0; i < tagInstances.length; i++) {
-            fn(tagInstances[i]);
-        }
+
+// This rickrolls your HTML element lol. For a button: <button onclick="rickrollElementById('demo');" id="demo">Click Me!</button>
+function rickrollElementById(rickroll) {
+document.getElementById(rickroll).innerHTML = '<iframe width="1280" height="720" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
- 
-function lol(element){
-        //code for rendering the element goes here
-// W.I.P tag
-        }
-}   
- 
-customTag("lol",lol);
