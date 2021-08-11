f19905c2f9e61af2e5e775f79e1b6bfea46457a4
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-08-11 17:36:37 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,8 +2,8 @@
 const _pi = 3.141;
 const _2b2tseed = -4172144997902289642;
 
-
-// This rickrolls your HTML element lol. For a button: <button onclick="rickrollElementById('demo');" id="demo">Click Me!</button>
+// This replaces your HTML element with an iframe showing rickroll. For a button: <button onclick="rickrollElementById('demo');" id="demo">Click Me!</button>
+// How to use: use double or single quotes for the element id: rickrollElementById('element id'); or rickrollElementById("element id");
 function rickrollElementById(rickroll) {
 document.getElementById(rickroll).innerHTML = '<iframe width="1280" height="720" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
